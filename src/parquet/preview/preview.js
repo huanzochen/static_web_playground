de7be7ae7e0213a50cdde409dbb7b4008905bd11
tzuchuan.huang <cy94295@gmail.com>
2025-02-22 15:12:43 +0800

    feat: load parquet data demo

--- src/parquet/preview/preview.js
+++ src/parquet/preview/preview.js
@@ -8,6 +8,20 @@ async function initializeParquetWasm() {
 
 await initializeParquetWasm();
 
+const page = document.getElementById("page");
+const output = document.getElementById("output")
+
+let offset = 0;
+const pageSize = 1000
+let totalRows
+let table
+
+const updateUI = () => {
+  console.log(`正在載入第 ${offset / pageSize + 1} 頁...`);
+  output.innerText = `正在載入第 ${offset / pageSize + 1} 頁...`
+  page.innerText = `第 ${offset / pageSize + 1} 頁 `
+}
+
 async function loadParquet() {
   // 讀取 parquet 檔案
   const response = await fetch("/10Mpoints.parquet");
@@ -24,27 +38,41 @@ async function loadParquet() {
 
   // 轉換為 JS Arrow Table
   import("apache-arrow").then(arrow => {
-    const table = arrow.tableFromIPC(wasmTable.intoIPCStream());
+    table = arrow.tableFromIPC(wasmTable.intoIPCStream());
+    totalRows = table.numRows
 
     // 取得欄位名稱
     console.log("Schema:", table.schema.toString());
 
-    // 逐行讀取 x, y 欄位
-    const rows = [];
-    for (let i = 0; i < Math.min(10_000, table.numRows); i++) {
-      const x = table.getChild("x")?.get(i);
-      const y = table.getChild("y")?.get(i);
-      rows.push({ x, y });
-    }
-
-    console.log("讀取的資料：", rows);
-    
-    // 顯示結果
-    document.getElementById("output").textContent = "Loaded Done"
+    loadData()
   });
 }
 
+const loadData = () => {
+  // 逐行讀取 x, y 欄位
+  const rows = [];
+  for (let i = offset; i < Math.min(offset + pageSize, table.numRows); i++) {
+    const x = table.getChild("x")?.get(i);
+    const y = table.getChild("y")?.get(i);
+    rows.push({ x, y });
+  }
+  // 顯示結果
+  output.textContent = JSON.stringify(rows, null, 2)
+  console.log("讀取的資料：", rows);
+}
+
+// 更新 UI
+updateUI()
 // 執行讀取
 loadParquet().catch(error => {
   console.error("讀取 Parquet 失敗：", error);
 });
+
+
+document.getElementById("next").addEventListener("click", () => {
+  offset += pageSize
+  updateUI()
+  loadData()
+})
+
+
