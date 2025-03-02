e3cae602763ebb99b7fd69039a0fa457735c4090
tzuchuan.huang <cy94295@gmail.com>
2025-03-03 00:09:39 +0800

    feat: canvas_drawer part1

--- src/parquet/canvas_draw/index.js
+++ src/parquet/canvas_draw/index.js
@@ -2,10 +2,25 @@ const worker = new Worker(new URL('./parquetWorker.js', import.meta.url), {
   type: 'module',
 })
 
-const page = document.getElementById('page')
-const output = document.getElementById('output')
+const randomColor = [
+  '#5C7285',
+  '#818C78',
+  '#A7B49E',
+  '#E2E0C8',
+  '#E6B2BA',
+  '#FAD0C4',
+]
 
-const PAGE_SIZE = 1000
+const output = document.getElementById('output')
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+canvas.width = window.innerWidth - 10
+canvas.height = window.innerWidth - 10
+const width = canvas.width
+const height = canvas.height
+
+const PAGE_SIZE = 100_000
 let offset = 0
 let totalRows
 
@@ -15,12 +30,31 @@ let stopAutoLoad = false
 const updateLoadingUI = () => {
   console.log(`正在載入第 ${offset / PAGE_SIZE + 1} 頁...`)
   output.textContent = `正在載入第 ${offset / PAGE_SIZE + 1} 頁...`
-  page.innerText = `第 ${offset / PAGE_SIZE + 1} 頁 `
+}
+
+const getRandomColor = () => {
+  const rand = Math.floor(Math.random() * randomColor.length)
+  return randomColor[rand]
+}
+
+const scale = (value, min, max, newMin, newMax) => {
+  return ((value - min) / (max - min)) * (newMax - newMin) + newMin
 }
 
 const updateContent = (content) => {
-  output.textContent = content
+  content.forEach((el) => {
+    const scaledX = scale(el.x, 0, 1000, 0, width)
+    const scaledY = scale(el.y, 0, 1000, 0, height)
+
+    ctx.beginPath()
+    ctx.arc(scaledX, scaledY, 5, 0, 2 * Math.PI)
+    ctx.fillStyle = getRandomColor()
+    ctx.fill()
+  })
+  loadNext()
 }
+
+updateLoadingUI()
 
 async function loadParquetFile() {
   try {
@@ -71,33 +105,12 @@ worker.onmessage = (event) => {
   if (action === 'auto data') {
     const { content } = event.data
     updateContent(content)
-    if (!stopAutoLoad) {
-      autoLoadTimeoutId = setTimeout(() => {
-        loadNext()
-      }, 100)
-    }
   }
 
   if (action === 'error') {
     console.error('worker 讀取失敗:', error)
   }
 }
-
-// 更新 UI
-updateLoadingUI()
-
-document.getElementById('next').addEventListener('click', () => {
-  offset += PAGE_SIZE
-  updateLoadingUI()
-  worker.postMessage({ action: 'loadData', offset, PAGE_SIZE })
-})
-
-document.getElementById('previous').addEventListener('click', () => {
-  if (offset === 0) return
-  offset -= PAGE_SIZE
-  updateLoadingUI()
-  worker.postMessage({ action: 'loadData', offset, PAGE_SIZE })
-})
 
 const loadNext = () => {
   offset += PAGE_SIZE
